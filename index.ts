3c8d68c2e0453d46646e644955274f00e55494db
Trevor Baron <trevorjbaron@gmail.com>
2018-11-07 19:47:31 -0800

    working windows

--- index.ts
+++ index.ts
@@ -1,12 +1,44 @@
 import glsl from "./nodeSrc/glslangValidator"
+import * as ipc from "node-ipc"
 var app = require('./nativeSrc/build/Release/app');
+
+const { fork } = require('child_process');
+
+
+// var main = async ()=>{
+//     console.log("Compiling shaders ------------------------------------------------------")
+//     await glsl.compileShader("shaders/shader.vert", "shaders/vert.spv")
+//     await glsl.compileShader("shaders/shader.frag", "shaders/frag.spv")
+//     console.log("Launching app ------------------------------------------------------")
+//     app.init("Hello World")
+//     app.render("Hello World")
+// }
+// main().catch((e)=>{
+//     console.error(e)
+// })
 
 var main = async ()=>{
     console.log("Compiling shaders ------------------------------------------------------")
     await glsl.compileShader("shaders/shader.vert", "shaders/vert.spv")
     await glsl.compileShader("shaders/shader.frag", "shaders/frag.spv")
-    console.log("Launching app ------------------------------------------------------")
-    app.run("Hello World")
+    console.log("Launching SERVER ------------------------------------------------------")
+    var handle = app.init("");
+    //app.init2(handle);
+    
+    console.log("server handle: "+handle)
+
+    ipc.config.id = 'compositor';
+    ipc.config.retry = 1500;
+    ipc.config.silent = true;
+    ipc.serve(() => {
+        ipc.server.on('requestImgHandle', (message:string, socket) => {
+            ipc.server.emit(socket, "imgHandle", {handle: handle})
+            app.render("")
+        })
+    });
+    ipc.server.start();
+
+    const command = fork('client.ts');
 }
 main().catch((e)=>{
     console.error(e)
--- nativeSrc/app/wrapper.cpp
+++ nativeSrc/app/wrapper.cpp
@@ -1,13 +1,63 @@
 #include <napi.h>
-#include "lib.cpp"
 
-void Method(const Napi::CallbackInfo& info) {
+#include <iostream>
+#include <string>
+#include "vulkan.cpp"
+
+HelloTriangleApplication app;
+
+
+Napi::Number InitVulkan(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
   auto str = info[0].As<Napi::String>();
-  main(str);
+  unsigned long handle = 0;
+  try {
+      app.initWindow();
+      auto x = app.initVulkan();
+      //app.createTextureImageExt(x);
+      app.initVulkan2();
+      handle = (unsigned long)x;
+
+  } catch (const std::exception& e) {
+      std::cerr << e.what() << std::endl;
+  }
+
+  // const int n = snprintf(NULL, 0, "%lu", handle);
+  // assert(n > 0);
+  // char buf[50];//n+1];
+  // int c = snprintf(buf, n+1, "%lu", handle);
+  // assert(buf[n] == '\0');
+  // assert(c == n);
+
+  return Napi::Number::New(env, handle);
+}
+
+void InitVulkan2(const Napi::CallbackInfo& info) {
+  try {
+    Napi::Env env = info.Env();
+    auto number = info[0].As<Napi::Number>();
+    HANDLE handle = (HANDLE)number.Uint32Value();
+    //std::cerr << handle << std::endl;
+    app.createTextureImageExt(handle);
+  } catch (const std::exception& e) {
+      std::cerr << e.what() << std::endl;
+  }
+}
+
+void Render(const Napi::CallbackInfo& info) {
+  auto str = info[0].As<Napi::String>();
+  try {
+      app.mainLoop();
+      app.cleanup();
+  } catch (const std::exception& e) {
+      std::cerr << e.what() << std::endl;
+  }
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports.Set(Napi::String::New(env, "run"), Napi::Function::New(env, Method));
+  exports.Set(Napi::String::New(env, "init"), Napi::Function::New(env, InitVulkan));
+  exports.Set(Napi::String::New(env, "init2"), Napi::Function::New(env, InitVulkan2));
+  exports.Set(Napi::String::New(env, "render"), Napi::Function::New(env, Render));
   return exports;
 }
 
